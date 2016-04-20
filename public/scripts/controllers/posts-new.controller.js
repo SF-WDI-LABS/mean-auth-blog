14d867943220b4bdc1585e9975a133fb685f6fc0
nathanallen <thenathanator@gmail.com>
2016-04-20 11:13:39 -0700

    strip out controller code

--- public/scripts/controllers/posts-new.controller.js
+++ public/scripts/controllers/posts-new.controller.js
@@ -1,22 +1,5 @@
 PostsNewController.$inject = ["$location", "$http"]; // minification protection
 function PostsNewController ($location, $http) {
   var vm = this;
-  vm.create = create;
   vm.post = {}; // form data
-
-  ////
-
-  function create() {
-    $http
-      .post('/api/posts', vm.post)
-      .then(onCreateSuccess, onCreateError);
-
-    function onCreateSuccess(response){
-      $location.path('/posts/' + response.data._id)
-    }
-
-    function onCreateError(response){
-      console.error("Failed to create post", response);
-    }
-  };
 }
--- public/scripts/controllers/posts-show.controller.js
+++ public/scripts/controllers/posts-show.controller.js
@@ -2,24 +2,4 @@ PostsShowController.$inject = ["$location", "$http", "$routeParams"]; // minific
 function PostsShowController ($location, $http, $routeParams) {
   var vm = this;
   vm.post = {};
-
-  var id = $routeParams.id;
-  get(); // fetch one post (show)
-
-  ////
-
-  function get() {
-    $http
-      .get('/api/posts/' + id)
-      .then(onGetSuccess, onGetError);
-
-    function onGetSuccess(response){
-      vm.post = response.data;
-    }
-
-    function onGetError(response){
-      console.error("Failed to get post", response);
-      $location.path("/");
-    }
-  };
 }
